d327888b8e09b6a4914b6139b356b16f756be387
Thiago Mantoan de Godoi <65636214+mantoan-thi@users.noreply.github.com>
2022-04-05 20:58:50 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+from distutils.command.config import config
+from flask import Flask,request,jsonify
+from flask_basicauth import BasicAuth
+from textblob import TextBlob
+from sklearn.linear_model import LinearRegression
+import pickle
+
+modelo = pickle.load(open('modelo.sav','rb'))
+
+colunas = ['tamanho','ano','garagem']
+
+app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'Mantoan'
+app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+
+basic_auth = BasicAuth(app)
+
+@app.route('/')
+def home():
+    return "Minha primeira API."
+
+@app.route('/sentimento/<frase>')
+@basic_auth.required
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(to='en')
+    polaridade = tb_en.sentiment.polarity
+    return "polaridade: {}".format(polaridade)
+
+@app.route('/cotacao/',methods=['POST'])
+@basic_auth.required
+def cotacao():
+    dados = request.get_json()
+    dados_input = [dados[col] for col in colunas]
+    preco = modelo.predict([dados_input])
+    return jsonify(preco=preco[0])
+
+app.run(debug=True)
